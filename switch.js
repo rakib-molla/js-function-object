b12278387e34311247df1bd5bcc208e48a2fc9f9
rakib-molla <rakibmail1997@gmail.com>
2023-01-30 00:38:06 +0600

    switch statement added

---
+++ switch.js
@@ -0,0 +1,28 @@
+
+const color = "white";
+if(color == 'red'){
+    console.log('you ar a red friend');
+}else if(color == 'green'){
+    console.log('you ar a green friend');
+}else if(color == 'purple'){
+    console.log('you ar a purple friend');
+}else if(color == 'gray'){
+    console.log('you ar a gray friend');
+}else if(color == 'yellow'){
+    console.log('you ar a yellow friend');
+}else if(color == 'white'){
+    console.log('you ar a white friend');
+}
+
+// switch statement 
+switch(color){
+    case 'green':
+        console.log( 'you are my green friend');
+        break;
+    case 'blue':
+        console.log('you are my blue friend');
+        break;
+    case 'white':
+        console.log('you are my white friend');
+        break;
+}
